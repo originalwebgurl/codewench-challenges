c922ccd658a5e8fd50cef763e800809a595d0cda
Sherri Flemings <sherri@battlesnake.com>
2021-10-01 11:02:24 -0400

    Code cleanup

--- src/food.js
+++ src/food.js
@@ -11,7 +11,6 @@ function safeMoves(head, board) {
     for(let y = 0; y<board.food.length; y++) {
         const pickup = board.food[y];
 
-        //console.warn("pickup", pickup, head, board);
         if(pickup.x < head.x) {
             temp.push({dir: "left", dist: head.x - pickup.x});
         }
@@ -30,13 +29,9 @@ function safeMoves(head, board) {
     // sort by distance
     if(temp.length > 0)
     {
-        // console.warn("BEFORE SORT", temp);
-
         temp.sort((a, b) => {
             return a.dist - b.dist;
         });
-
-        // console.warn("AFTER SORT", temp);
     }
 
     possibleMoves = temp.map(move => move.dir);
